3aab8f80aa957054827fccbba46b4ae8ddd0160d
Muhammad Abdullah <ma22052000@gmail.com>
2023-09-08 18:52:24 +0700

    tambah fungsi add Buku

--- baru.go
+++ baru.go
@@ -154,8 +154,53 @@ func getBuku(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{"data": data})
 }
 
-func addBuku(c *gin.Context){
-    //
+func addBuku(c *gin.Context) {
+    var requestBody struct {
+        Judul    string `json:"judul"`
+        Tahun string `json:"tahun"`
+    }
+
+    if err := c.ShouldBindJSON(&requestBody); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+	url := "http://127.0.0.1:8000/api/buku/store"
+
+	data := strings.NewReader(fmt.Sprintf(`{"judul": "%s", "tahun": "%s"}`, requestBody.Judul, requestBody.Tahun))
+    req, err := http.NewRequest("POST", url, data)
+    if err != nil {
+        fmt.Println("Gagal membuat permintaan HTTP:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal membuat permintaan HTTP"})
+        return
+    }
+    req.Header.Set("Content-Type", "application/json")
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        fmt.Println("Gagal melakukan permintaan HTTP:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal melakukan permintaan HTTP"})
+        return
+    }
+    defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+        fmt.Println("Gagal menambahkan data, status kode:", resp.StatusCode)
+        c.JSON(http.StatusBadGateway, gin.H{"error": "Gagal menambahkan data"})
+        return
+    }
+
+	body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Gagal membaca respons:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal membaca respons"})
+        return
+    }
+
+    data_add := string(body)
+    c.JSON(http.StatusCreated, gin.H{"data": data_add})
 }
 
 func getBukuByID(c *gin.Context) {
